f86d91f174d3a5f5324612822eea61db6fe5e264
Zeineb Dridi <zeineb.dridi@medtech.tn>
2021-09-27 11:22:40 +0100

    Modified index and js

--- StudentManagementSystem.js
+++ StudentManagementSystem.js
@@ -1,1 +1,32 @@
+var numberOfStudent = 0 
+var students = []
+function addStudent (name) {
+   students.push(name) 
+   return numberOfStudent++
+   
+}
+function getNumberOfStudent() {
+    return numberOfStudent
+}
+addStudent('Zeineb')
+addStudent('Kooli')
+addStudent('karoui')
+addStudent('belghith')
+addStudent('rayen')
 
+function clearStudents (){
+    for (i=0;i<5;i++){
+    students.pop()
+    numberOfStudent-- }
+} 
+
+function createFullName (firstName, lastName){
+    return 'my name is: $(firstName) $(lastName)' 
+}
+createFullName ('Ahmed', 'Belghith')
+
+console.log(students) 
+getNumberOfStudent() 
+clearStudents()
+console.log(students)
+getNumberOfStudent()
